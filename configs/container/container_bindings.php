740ea777e345ffb1bc884c01bbbaff5dd252aa3f
Adil Tafsast <adil.tafs@gmail.com>
2024-04-03 12:13:19 +0200

    Integrate Webpack using Symfony Webpack Encore

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -6,13 +6,20 @@ use App\Config;
 use App\Enum\AppEnvironment;
 use Psr\Container\ContainerInterface;
 use Slim\Views\Twig;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
+use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
+use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
 use Twig\Extra\Intl\IntlExtension;
 
 use function DI\create;
 
 return [
-    Config::class => create(Config::class)->constructor(require CONFIG_PATH . '/app.php'),
-
+    Config::class                 => create(Config::class)->constructor(require CONFIG_PATH . '/app.php'),
     Twig::class                   => function (Config $config, ContainerInterface $container) {
         $twig = Twig::create(VIEW_PATH, [
             'cache'       => STORAGE_PATH . '/cache/templates',
@@ -20,7 +27,19 @@ return [
         ]);
 
         $twig->addExtension(new IntlExtension());
+        $twig->addExtension(new EntryFilesTwigExtension($container));
+        $twig->addExtension(new AssetExtension($container->get('webpack_encore.packages')));
 
         return $twig;
     },
+    /**
+     * The following two bindings are needed for EntryFilesTwigExtension & AssetExtension to work for Twig
+     */
+    'webpack_encore.packages'     => fn() => new Packages(
+        new Package(new JsonManifestVersionStrategy(BUILD_PATH . '/manifest.json'))
+    ),
+    'webpack_encore.tag_renderer' => fn(ContainerInterface $container) => new TagRenderer(
+        new EntrypointLookup(BUILD_PATH . '/entrypoints.json'),
+        $container->get('webpack_encore.packages')
+    ),
 ];
