ecdca1ac8188e3ba5ad4af345ed53ab011ec517b
Adil Tafsast <adil.tafs@gmail.com>
2024-04-23 15:39:32 +0200

    Improve data table accessibility with HTML third-party library DataTables

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,19 +1,72 @@
-import { Modal }     from "bootstrap"
+import { Modal }          from "bootstrap"
 import { get, post, del } from "./ajax"
+import DataTable          from "datatables.net"
 
 window.addEventListener('DOMContentLoaded', function () {
     const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
+    const newCategoryModal = new Modal(document.getElementById('newCategoryModal'))
 
-    document.querySelectorAll('.edit-category-btn').forEach(button => {
-        button.addEventListener('click', function (event) {
-            const categoryId = event.currentTarget.getAttribute('data-id')
+    const table = new DataTable('#categoriesTable', {
+        serverSide: true,
+        ajax: '/categories/load',
+        orderMulti: false,
+        columns: [
+            {data: "name"},
+            {data: "createdAt"},
+            {data: "updatedAt"},
+            {
+                sortable: false,
+                data: row => `
+                    <div class="d-flex flex-">
+                        <button type="submit" class="btn btn-outline-primary delete-category-btn" data-id="${ row.id }">
+                            <i class="bi bi-trash3-fill"></i>
+                        </button>
+                        <button class="ms-2 btn btn-outline-primary edit-category-btn" data-id="${ row.id }">
+                            <i class="bi bi-pencil-fill"></i>
+                        </button>
+                    </div>
+                `
+            }
+        ]
+    });
+
+    document.querySelector('.new-category-btn').addEventListener('click', function (event) {
+        const categoryId = event.currentTarget.getAttribute('data-id')
+
+        post(`/categories`, {
+            name: newCategoryModal._element.querySelector('input[name="name"]').value
+        }, newCategoryModal._element).then(response => {
+            if (response.ok) {
+                newCategoryModal._element.querySelector('input[name="name"]').value = "";
+                table.draw()
+                newCategoryModal.hide()
+            }
+        })
+    })
+    
+    document.querySelector('#categoriesTable').addEventListener('click', function (event) {
+        const editBtn   = event.target.closest('.edit-category-btn')
+        const deleteBtn = event.target.closest('.delete-category-btn')
+
+        if (editBtn) {
+            const categoryId = editBtn.getAttribute('data-id')
 
             get(`/categories/${ categoryId }`)
                 .then(response => response.json())
                 .then(response => openEditCategoryModal(editCategoryModal, response))
-        })
-    })
+        } else {
+            const categoryId = deleteBtn.getAttribute('data-id')
 
+            if (confirm('Are you sure you want to delete this category?')) {
+                del(`/categories/${ categoryId }`).then(response => {
+                    if (response.ok) {
+                        table.draw()
+                    }
+                })
+            }
+        }
+    })
+    
     document.querySelector('.save-category-btn').addEventListener('click', function (event) {
         const categoryId = event.currentTarget.getAttribute('data-id')
 
@@ -21,21 +74,11 @@ window.addEventListener('DOMContentLoaded', function () {
             name: editCategoryModal._element.querySelector('input[name="name"]').value
         }, editCategoryModal._element).then(response => {
             if (response.ok) {
+                table.draw()
                 editCategoryModal.hide()
             }
         })
     })
-
-    document.querySelectorAll('.delete-category-btn').forEach(button => {
-        button.addEventListener('click', function (event) {
-            const categoryId = event.currentTarget.getAttribute('data-id')
-
-            if (confirm('Are you sure you want to delete this category?')) {
-                del(`/categories/${ categoryId }`)
-            }
-        })
-    })
-
 })
 
 function openEditCategoryModal(modal, {id, name}) {
